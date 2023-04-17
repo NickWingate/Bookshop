f1e0b206f7410921b1885641e9eeef22b514d50b
NickWingate <nicholas.wingate03@gmail.com>
2023-04-17 19:03:49 +0100

    Add search functionality

--- src/main/java/ui/controllers/HomeViewController.java
+++ src/main/java/ui/controllers/HomeViewController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import main.java.domain.entities.AudioBook;
 import main.java.domain.entities.Book;
 import main.java.domain.enums.AudioFormat;
@@ -14,6 +15,7 @@ import main.java.ui.controls.BookCollectionControl;
 import main.java.util.file.BookEncoder;
 import main.java.util.file.BookParser;
 import main.java.util.file.CSVWriter;
+import main.java.util.interfaces.IBookRepository;
 import main.java.util.repositories.BookRepository;
 
 import java.math.BigDecimal;
@@ -23,32 +25,61 @@ import java.util.ResourceBundle;
 
 public class HomeViewController implements Initializable {
 
+    private IBookRepository _bookRepository;
     @FXML
     private BookCollectionControl bookCollectionControl;
 
     @FXML
     private Button addBook;
 
+    @FXML
+    private Button search;
+
+    @FXML
+    private TextField searchField;
+
     private ObservableList<Book> books = FXCollections.observableArrayList();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        var bookRepo = new BookRepository(new BookParser(),
+        _bookRepository = new BookRepository(new BookParser(),
                 new CSVWriter<Book>(new BookEncoder()),
                 "src/main/resources/Stock.txt");
-        books.addAll(bookRepo.GetAll());
+        books.addAll(_bookRepository.GetAll());
 
-        addBook.setOnAction(actionEvent -> {
-            books.add(new AudioBook(
-                    "11224455",
-                    "A Promised Land",
-                    Language.ENGLISH,
-                    Genre.BIOGRAPHY,
-                    LocalDate.of(2020, 11, 24),
-                    10,
-                    new BigDecimal("30.25"),
-                    4.5f,
-                    AudioFormat.MP3));
-        });
+//        addBook.setOnAction(actionEvent -> {
+//            books.add(new AudioBook(
+//                    "11224455",
+//                    "A Promised Land",
+//                    Language.ENGLISH,
+//                    Genre.BIOGRAPHY,
+//                    LocalDate.of(2020, 11, 24),
+//                    10,
+//                    new BigDecimal("30.25"),
+//                    4.5f,
+//                    AudioFormat.MP3));
+//        });
+
+        search.setOnAction(actionEvent -> searchBooks());
+    }
+
+    private void searchBooks() {
+        books.clear();
+        var searchTerm = searchField.getText();
+
+        var allBooks = _bookRepository.GetAll();
+
+        if (searchTerm.equals("")) {
+            books.addAll(allBooks);
+            return;
+        }
+
+        for (var book : allBooks) {
+            if (book.getTitle().toLowerCase().contains(searchTerm.toLowerCase())){
+                books.add(book);
+            }
+        }
+
+
     }
 
     public ObservableList<Book> getBooks() {
